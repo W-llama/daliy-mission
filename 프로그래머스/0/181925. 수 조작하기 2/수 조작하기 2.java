e6e00da4cf032b4f500ef7e679d6463ea058d661
W.llama <106076670+W-llama@users.noreply.github.com>
2024-09-03 15:50:31 +0900

    [level 0] Title: 수 조작하기 2, Time: 1109.03 ms, Memory: 372 MB -BaekjoonHub

--- 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -1,15 +1,13 @@
 class Solution {
     public String solution(int[] numLog) {
         String answer = "";
-        for(int i = 1; i < numLog.length; i++){
-            if(numLog[i] - numLog[i - 1] == 1){
-                answer +="w";
-            }else if(numLog[i] - numLog[i - 1] == -1){
-                answer +="s";
-            }else if(numLog[i] - numLog[i - 1] == 10){
-                answer +="d";
-            }else if(numLog[i] - numLog[i - 1] == -10){
-                answer +="a";
+        for(int i=1; i<numLog.length; i++){
+            int j = numLog[i-1] - numLog[i];
+            switch(j){
+                case -1 : answer+='w'; break;
+                case 1 : answer+='s'; break;
+                case -10 : answer+='d'; break;
+                case 10 : answer+='a'; break;
             }
         }
         return answer;
