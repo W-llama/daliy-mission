eb09bc6e227d6053e5dda4737cc3f5683f29f64d
W.llama <106076670+W-llama@users.noreply.github.com>
2024-09-09 22:59:45 +0900

    [level 0] Title: 카운트 업, Time: 0.03 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] solution(int start_num, int end_num) {
+        int length = end_num - start_num;
+        int[] answer = new int [length + 1];
+        for(int i = 0; i <= length; i++){
+            answer[i] =i+ start_num;
+        }
+        return answer;
+    }
+}
