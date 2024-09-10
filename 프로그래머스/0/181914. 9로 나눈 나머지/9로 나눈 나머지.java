009342dca519799e88b797dac132771b77d9f5d6
W.llama <106076670+W-llama@users.noreply.github.com>
2024-09-10 21:00:15 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 5.03 ms, Memory: 84.8 MB -BaekjoonHub

--- 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -2,7 +2,7 @@ class Solution {
     public int solution(String number) {
         int answer = 0;
         for (char digit : number.toCharArray()){
-            answer += Character.getNumericValue(digit);
+            answer += digit - '0';
         }
         return answer % 9;
     }
