b51282697f175c59ee9212737c8b3bbb57bbe598
W.llama <106076670+W-llama@users.noreply.github.com>
2024-12-25 18:46:32 +0900

    [Bronze V] Title: A+B, Time: 172 ms, Memory: 17700 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
+public class Main{
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         
-        int A = sc.nextInt();
-        int B = sc.nextInt();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
         
-        int sum = A+B;
-
-        System.out.println(sum);
+        int answer = a + b;
+        
+        System.out.println(answer);
     }
 }
