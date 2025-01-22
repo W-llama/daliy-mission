6422dff63023f5e30ddcdb793831e2bf572b1359
W.llama <106076670+W-llama@users.noreply.github.com>
2025-01-22 11:00:22 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 104 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -1,26 +1,29 @@
-import java.util.Scanner;
- 
-public class Main {
- 
-	public static void main(String[] args) {
- 
-		Scanner in = new Scanner(System.in);
-		
-		int a = in.nextInt();
-		int b = in.nextInt();
- 
-		int d = gcd(a, b);
- 
-		System.out.println(d);
-		System.out.println(a * b / d);
- 
-	}
- 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-	public static int gcd(int a, int b) {
-		if (b == 0)
-			return a;
- 
-		return gcd(b, a % b);
-	}
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int num1 = Integer.parseInt(st.nextToken());
+        int num2 = Integer.parseInt(st.nextToken());
+
+        int GCD = getGCD(num1,num2);
+
+        System.out.println(GCD);
+        System.out.println(num1*num2/GCD);
+
+    }
+
+    public static int getGCD(int num1, int num2){
+        if(num2 == 0){
+            return num1;
+        }else {
+            return getGCD(num2, num1 % num2);
+        }
+    }
 }
