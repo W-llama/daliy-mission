629280b5b31a3570ea4c271f9544906d4e412acb
W.llama <106076670+W-llama@users.noreply.github.com>
2025-03-13 22:09:41 +0900

    [level 0] Title: A 강조하기, Time: 0.13 ms, Memory: 81.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        
+        myString = myString.toLowerCase();
+        myString = myString.replaceAll("a", "A");
+        answer = myString;
+        return answer;
+    }
+}
