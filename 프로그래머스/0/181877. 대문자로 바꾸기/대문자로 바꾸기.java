94824cf03e31f7462d7297a3deafb443de047ed1
W.llama <106076670+W-llama@users.noreply.github.com>
2025-03-13 21:23:13 +0900

    [level 0] Title: 대문자로 바꾸기, Time: 0.03 ms, Memory: 84.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181877. 대문자로 바꾸기/대문자로 바꾸기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        String str = myString.toUpperCase();
+        answer = str;
+        return answer;
+    }
+}
