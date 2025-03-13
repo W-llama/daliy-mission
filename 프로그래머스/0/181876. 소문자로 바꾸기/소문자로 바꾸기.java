1544200b6ff9296e74abb0f66394b4e01f6471b5
W.llama <106076670+W-llama@users.noreply.github.com>
2025-03-13 21:24:03 +0900

    [level 0] Title: 소문자로 바꾸기, Time: 0.04 ms, Memory: 88.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181876. 소문자로 바꾸기/소문자로 바꾸기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        String str = myString.toLowerCase();
+        answer = str;
+        return answer;
+    }
+}
